8f913ac088099c522a11f756bcd8fa33b5628e74
Coder731 <stephenobrien1@gmail.com>
2021-08-06 13:49:03 +0000

    Import and Debug fixed failed to compile error

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Build-a-News-App-With-React-React-Tutorial-For-Beginners-following-tutorial
 https://www.youtube.com/watch?v=jTk6XP4IgGs
+
+
+## Comments
+### What is use State and what does it do?
+- A hook.
+- Enable us to track the state of our RTeact component
--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react';
+import "./App.css";
 
-import './App.css';
+const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=08aa08ce867c4d2eae07a4b968d1874c"
+
 
 function App() {
+  const  [data,setData] = useState({})
   return (
     <div className="App">
       <h1>Welcome to News App</h1>
